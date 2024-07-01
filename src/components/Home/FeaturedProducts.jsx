cd8bdc892f3a69cba681020be5e2e76a190fef0f
Akash-Chhangani <akashchhangani4@gmail.com>
2024-07-01 15:04:28 +0530

    Created Product Card

--- src/components/Home/FeaturedProducts.jsx
+++ src/components/Home/FeaturedProducts.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./FeaturedProducts.css";
+import ProductCard from "./ProductCard";
 const FeaturedProducts = () => {
   return (
     <section className="featured_products">
       <h2>Feature Products</h2>
       <div className="align_center featured_products_list">
-        <article className="product_card">Products</article>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
       </div>
     </section>
   );
