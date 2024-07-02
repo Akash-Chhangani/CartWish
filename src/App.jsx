59f48b24de5c25c151e701c8a5bdcbddba2885f4
Akash-Chhangani <akashchhangani4@gmail.com>
2024-07-02 16:34:29 +0530

    Build CartPage Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./components/Home/HomePage";
 import ProductsPage from "./components/Products/ProductsPage";
 import SingleProductPage from "./components/SingleProduct/SingleProduct";
+import CartPage from "./components/Cart/CartPage";
 
 const App = () => {
   return (
@@ -11,7 +12,8 @@ const App = () => {
       <Navbar />
       {/* <HomePage /> */}
       {/* <ProductsPage /> */}
-      <SingleProductPage />
+      {/* <SingleProductPage /> */}
+      <CartPage />
     </div>
   );
 };
