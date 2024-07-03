c33cf93c4ee80f74b24dbe18a1f8e7e613d5e63d
Akash-Chhangani <akashchhangani4@gmail.com>
2024-07-03 13:40:49 +0530

    Implemnting Routing in our Application by installing react-router-dom labiray

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import HomePage from "./components/Home/HomePage";
-import ProductsPage from "./components/Products/ProductsPage";
-import SingleProductPage from "./components/SingleProduct/SingleProduct";
-import CartPage from "./components/Cart/CartPage";
-import MyOrderPage from "./components/MyOrder/MyOrderPage";
-import LoginPage from "./components/Authentication/LoginPage";
-import SignupPage from "./components/Authentication/SignupPage";
+import Routing from "./components/Routing/Routing";
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
-      {/* <HomePage /> */}
-      {/* <ProductsPage /> */}
-      {/* <SingleProductPage /> */}
-      {/* <CartPage /> */}
-      {/* <MyOrderPage /> */}
-      {/* <LoginPage /> */}
-      <SignupPage />
+      <main>
+        <Routing />
+      </main>
     </div>
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,6 +7,7 @@ import memo from "../../assets/memo.png";
 import order from "../../assets/package.png";
 import lock from "../../assets/locked.png";
 import LinkWithIcon from "./LinkWithIcon";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="align_center navbar">
@@ -30,9 +31,9 @@ const Navbar = () => {
         <LinkWithIcon title="SignUP" link="/signup" emoji={memo} />
         <LinkWithIcon title="My Orders" link="/myorders" emoji={order} />
         <LinkWithIcon title="Log Out" link="/logout" emoji={lock} />
-        <a href="/cart" className="align_center">
+        <NavLink to="/cart" className="align_center">
           Cart <p className="align_center cart_counts">0</p>
-        </a>
+        </NavLink>
       </div>
     </nav>
   );
