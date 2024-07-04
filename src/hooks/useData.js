9f939f7f9d42be8d1d089524846e73ec9766fd87
Akash-Chhangani <akashchhangani4@gmail.com>
2024-07-04 12:09:43 +0530

    Fetching Products By Category

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,23 +1,26 @@
 import apiClient from "../utils/api-client";
 import { useState, useEffect } from "react";
 
-const useData = (url) => {
+const useData = (endpoint, customConfig, deps) => {
   const [data, setData] = useState(null);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    setIsLoading(true);
-    apiClient
-      .get(url)
-      .then((res) => {
-        setIsLoading(false);
-        setData(res.data);
-      })
-      .catch((err) => {
-        setIsLoading(false);
-        setError(err.message);
-      });
-  }, []);
+  useEffect(
+    () => {
+      setIsLoading(true);
+      apiClient
+        .get(endpoint, customConfig)
+        .then((res) => {
+          setIsLoading(false);
+          setData(res.data);
+        })
+        .catch((err) => {
+          setIsLoading(false);
+          setError(err.message);
+        });
+    },
+    deps ? deps : []
+  );
   return { data, error, isLoading };
 };
 
