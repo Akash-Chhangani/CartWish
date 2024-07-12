c9652649926db1f4aefef541a69b96bc5d9f401d
Akash-Chhangani <akashchhangani4@gmail.com>
2024-07-12 11:55:02 +0530

    Getting Users Orders

--- src/components/MyOrder/MyOrderPage.jsx
+++ src/components/MyOrder/MyOrderPage.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 import "./MyOrderPage.css";
+import "./MyOrderPage.css";
+import Table from "./../Table/Table";
+import useData from "../../hooks/useData";
+import Loader from "../Table/Loader";
+
 const MyOrderPage = () => {
+  const { data: orders, error, isLoading } = useData("/order");
+  const getProductString = (order) => {
+    const productStingArr = order.products.map(
+      (p) => `${p.product.title} (${p.quantity})`
+    );
+    return productStingArr.join(", ");
+  };
   return (
     <section className="align_center my_order_page">
-      <Table headings={["Order", "Products", "Total", "Status"]}>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>iPhone, Power Bank</td>
-            <td>$1299</td>
-            <td>Shipped</td>
-          </tr>
-        </tbody>
-      </Table>
+      {error && <em className="form_error">{error}</em>}
+      {isLoading && <Loader />}
+      {orders && (
+        <Table headings={["Order", "Products", "Total", "Status"]}>
+          <tbody>
+            {orders.map((order, index) => (
+              <tr key={order._id}>
+                <td>{index + 1}</td>
+                <td>{getProductString(order)}</td>
+                <td>${order.total}</td>
+                <td>{order.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
     </section>
   );
 };
-import "./MyOrderPage.css";
-import Table from "./../Table/Table";
 export default MyOrderPage;
